f241c3b4b64a63f1ee33efd30263b218f6b6c138
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-01-06 13:28:41 +0100

    Update ex02.py

--- python day 1/ex02.py
+++ python day 1/ex02.py
@@ -1,11 +1,1 @@
-def addi(U1,U2):
-    return U1 + U2
-
-def soustract(U1,U2):
-    return U1 - U2
-
-def mult(U1,U2):
-    return U1*U2
-
-def div(U1,U2):
-    return U1/U2
+print(eval(input('donne moi une expression à calculer : ')))
